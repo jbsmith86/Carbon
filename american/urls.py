4dd2eb3bd431cecfa1a63c9f4ce69d314af456d2
jbsmith86 <v-joelsm@FALCON42.redmond.corp.microsoft.com>
2013-08-14 07:01:34 -0700

    first

---
+++ american/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'american.views.home', name='home'),
+    # url(r'^american/', include('american.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$','blog.views.post'),
+)
