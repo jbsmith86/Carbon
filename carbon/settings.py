7b726b7f2d0397bd38b895b99e5e0c74d18389f8
Joel Smith <joely5525@gmail.com>
2014-04-15 23:54:51 -0700

    fixed header

--- carbon/settings.py
+++ carbon/settings.py
@@ -125,9 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'blog'
 )
